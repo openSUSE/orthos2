5546488acb43d5998cd5a0cbc3bfe8f6db10897c
Jan Löser <jan.loeser@posteo.de>
2020-07-06 07:58:40 +0200

    Allow disabling account creation
    By introducing `auth.account.creation` key the administrator is able to
    allow or disallow account creation. This may be relevant for upcoming
    LDAP authentication.

---
+++ orthos2/frontend/tests/user/test_ldap.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Author Jan Löser <jan.loeser@posteo.de>
+# Published under the GNU Public Licence 2
+
+from django.test import Client, TestCase
+from django.urls import reverse
+from django_webtest import WebTest
+
+from data.models.serverconfig import ServerConfig
+
+
+class LDAP(WebTest):
+
+    csrf_checks = True
+
+    fixtures = [
+        'frontend/tests/fixtures/serverconfigs.json',
+        'frontend/tests/user/fixtures/users.json'
+    ]
+
+    def test_enabled_user_creation(self):
+        """
+        Tests if user creation is enabled.
+        """
+        page = self.app.get(reverse('frontend:login'))
+        self.assertContains(page, 'Create Account')
+        self.assertContains(page, 'Restore Password')
+
+    def test_disabled_user_creation(self):
+        """
+        Tests if user creation is disabled.
+        """
+        creation = ServerConfig.objects.get(key='auth.account.creation')
+        creation.value = 'bool:false'
+        creation.save()
+
+        page = self.app.get(reverse('frontend:login'))
+        self.assertNotContains(page, 'Create Account')
+        self.assertNotContains(page, 'Restore Password')
+
+        form = self.app.get(reverse('frontend:create_user')).form
+        form['login'] = 'new-user'
+        form['email'] = 'new-user@example.com'
+        form['password'] = 'linux1234'
+        form['password2'] = 'linux1234'
+        page = form.submit().maybe_follow()
+
+        self.assertNotEqual(page.context['user'].username, 'new-user')
+        self.assertContains(page, 'Account creation is disabled!')
