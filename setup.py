84f8b6a21aae7e2bd070fc9d6cce4eb77cabcddb
Martin Rey <martin.rey@mailbox.org>
2020-09-25 11:48:31 +0200

    Format setup.py to line maxlength of 100 and improve docstrings

--- setup.py
+++ setup.py
@@ -4,35 +4,34 @@ from setuptools import find_packages, setup
 
 
 def requires(filename='requirements.txt'):
-    """Returns a list of all pip requirements
+    """Return a list of all pip requirements.
 
     :param filename: the Pip requirement file
-    (usually 'requirements.txt')
+                     (usually 'requirements.txt')
     :return: list of modules
     :rtype: list
     """
     with open(filename, 'r+t') as pipreq:
         for line in pipreq:
             line = line.strip()
-            if not line or \
-               line.startswith('#') or \
-               line.startswith('-r'):
+            if not line or line.startswith('#') or line.startswith('-r'):
                 continue
             yield line
 
 
 if __name__ == "__main__":
     setup(
-
         name="orthos2",
         version='0.1',
         description="Machine administration server",
-        long_description="Cobbler is a network install server. Cobbler supports PXE, virtualized installs, "
-                         "and reinstalling existing Linux machines. The last two modes use a helper tool, 'koan', "
-                         "that integrates with cobbler. There is also a web interface 'cobbler-web'. Cobbler's "
-                         "advanced features include importing distributions from DVDs and rsync mirrors, automatic OS "
-                         "installation templating, integrated yum mirroring, and built-in DHCP/DNS Management. "
-                         "Cobbler has a XMLRPC API for integration with other applications.",
+        long_description="Cobbler is a network install server. Cobbler supports PXE, virtualized "
+                         "installs, and reinstalling existing Linux machines. The last two modes "
+                         "use a helper tool, 'koan', that integrates with cobbler. There is also a"
+                         " web interface 'cobbler-web'. Cobbler's advanced features include "
+                         "importing distributions from DVDs and rsync mirrors, automatic OS "
+                         "installation templating, integrated yum mirroring, and built-in DHCP/DNS"
+                         " Management. Cobbler has a XMLRPC API for integration with other "
+                         "applications.",
         author="Team Cobbler",
         author_email="cobbler.project@gmail.com",
         url="https://cobbler.github.io",
@@ -45,6 +44,6 @@ if __name__ == "__main__":
             "cli/orthos2",
         ],
         data_files=[
-            ("/etc/nginx/conf.d",  ["orthos2_nginx.conf"])
+            ("/etc/nginx/conf.d", ["orthos2_nginx.conf"])
         ]
     )
