545486a836eab1ed87073349460bab15ce2ddfa4
Christian Vögl <cvoegl@suse.de>
2020-10-20 18:15:31 +0200

    new folder structure

--- orthos2/urls.py
+++ orthos2/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls import include, re_path
 from django.contrib import admin
 
 urlpatterns = [
-    re_path(r'^', include('frontend.urls')),
+    re_path(r'^', include('orthos2.frontend.urls', namespace='frontend')),
     re_path(r'^admin/', admin.site.urls),
-    re_path(r'^api/', include('api.urls', namespace='api')),
+    re_path(r'^api/', include('orthos2.api.urls', namespace='api')),
 ]
