e86ff4be63d82f3ad07af8edf4d8455c0c76f459
Thomas Renninger <trenn@suse.com>
2022-02-23 21:13:47 +0100

    'W0611' is cryptic: use '# pylint: disable=unused-import' instead

--- orthos2/bin/manage.py
+++ orthos2/bin/manage.py
@@ -11,8 +11,7 @@ if __name__ == "__main__":
         # issue is really that Django is missing to avoid masking other
         # exceptions on Python 2.
         try:
-            # pylint: disable=W0611
-            #  disable unused import warning
+            # pylint: disable=unused-import
             import django
         except ImportError:
             raise ImportError(
