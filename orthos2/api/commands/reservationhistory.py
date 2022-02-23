23298d35ec15e6763778cb64a2f570f6130d5738
Thomas Renninger <trenn@suse.com>
2022-02-23 16:26:48 +0100

    Convert old django.conf.url import to new django.url

--- orthos2/api/commands/reservationhistory.py
+++ orthos2/api/commands/reservationhistory.py
@@ -1,4 +1,4 @@
-from django.conf.urls import re_path
+from django.urls import re_path
 from django.http import HttpResponseRedirect, JsonResponse
 
 from orthos2.api.commands import BaseAPIView, get_machine
