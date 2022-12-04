2d17287f287322babb9ed9d45887130abbc8f57e
Thomas Renninger <trenn@suse.com>
2022-12-04 22:48:28 +0100

    Add whole info command path into exception handling
    so that always a valid json answer is given.
    Expecially here:
    response['data'] = machine.data_info
    
    class MachineSerializer(serializers.ModelSerializer)
    
    gets active and a bad DateTime value (will get fixed next) could
    result in an excpetion.
    
    bsc#1205857

--- orthos2/api/commands/info.py
+++ orthos2/api/commands/info.py
@@ -54,96 +54,96 @@ Example:
             elif isinstance(result, HttpResponseRedirect):
                 return result
             machine = result
-        except Exception as e:
-            return ErrorMessage(str(e)).as_json
 
-        machine.enclosure.fetch_location(machine.pk)
-        machine = MachineSerializer(machine)
+            machine.enclosure.fetch_location(machine.pk)
+            machine = MachineSerializer(machine)
 
-        order = [
-            'fqdn',
-            'architecture',
-            'ipv4',
-            'ipv6',
-            'serial_number',
-            'product_code',
-            'comment',
-            'nda',
-            None,
-            'system',
-            'enclosure',
-            'group',
-            None,
-            'location_room',
-            'location_rack',
-            'location_rack_position',
-            None,
-            'reserved_by',
-            'reserved_reason',
-            'reserved_at',
-            'reserved_until',
-            None,
-            'status_ipv4',
-            'status_ipv6',
-            'status_ssh',
-            'status_login',
-            None,
-            'cpu_model',
-            'cpu_id',
-            'cpu_physical',
-            'cpu_cores',
-            'cpu_threads',
-            'cpu_flags',
-            'ram_amount',
-            None,
-            'serial_type',
-            'serial_cscreen_server',
-            'serial_console_server',
-            'serial_port',
-            'serial_command',
-            'serial_comment',
-            'serial_baud_rate',
-            'serial_kernel_device',
-            'serial_kernel_device_num',
-            None,
-            'power_type',
-            'power_host',
-            'power_port',
-            'power_device',
-            'power_comment',
-            None,
-            'bmc_fqdn',
-            'bmc_mac',
-            'bmc_username',
-            'bmc_password',
-            [
-                'installations', [
-                    'distribution',
-                    'active',
-                    'partition',
-                    'architecture',
-                    'kernelversion'
-                ]
-            ],
-            [
-                'networkinterfaces', [
-                    'mac_address',
-                    'name',
-                    'ethernet_type',
-                    'driver_module',
-                    'primary',
-                ]
-            ],
-            [
-                'annotations', [
-                    'text',
-                    'reporter',
-                    'created'
+            order = [
+                'fqdn',
+                'architecture',
+                'ipv4',
+                'ipv6',
+                'serial_number',
+                'product_code',
+                'comment',
+                'nda',
+                None,
+                'system',
+                'enclosure',
+                'group',
+                None,
+                'location_room',
+                'location_rack',
+                'location_rack_position',
+                None,
+                'reserved_by',
+                'reserved_reason',
+                'reserved_at',
+                'reserved_until',
+                None,
+                'status_ipv4',
+                'status_ipv6',
+                'status_ssh',
+                'status_login',
+                None,
+                'cpu_model',
+                'cpu_id',
+                'cpu_physical',
+                'cpu_cores',
+                'cpu_threads',
+                'cpu_flags',
+                'ram_amount',
+                None,
+                'serial_type',
+                'serial_cscreen_server',
+                'serial_console_server',
+                'serial_port',
+                'serial_command',
+                'serial_comment',
+                'serial_baud_rate',
+                'serial_kernel_device',
+                'serial_kernel_device_num',
+                None,
+                'power_type',
+                'power_host',
+                'power_port',
+                'power_device',
+                'power_comment',
+                None,
+                'bmc_fqdn',
+                'bmc_mac',
+                'bmc_username',
+                'bmc_password',
+                [
+                    'installations', [
+                        'distribution',
+                        'active',
+                        'partition',
+                        'architecture',
+                        'kernelversion'
+                    ]
+                ],
+                [
+                    'networkinterfaces', [
+                        'mac_address',
+                        'name',
+                        'ethernet_type',
+                        'driver_module',
+                        'primary',
+                    ]
+                ],
+                [
+                    'annotations', [
+                        'text',
+                        'reporter',
+                        'created'
+                    ]
                 ]
             ]
-        ]
 
-        response['header'] = {'type': 'INFO', 'order': order}
-        response['data'] = machine.data_info
+            response['header'] = {'type': 'INFO', 'order': order}
+            response['data'] = machine.data_info
+        except Exception as e:
+            return ErrorMessage(getException()).as_json
 
         return JsonResponse(response)
