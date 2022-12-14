acac6aaa6a089e0b1c2d7bbf793efe41e0102abb
Thomas Renninger <trenn@suse.com>
2022-12-14 20:18:47 +0100

    Add hostname finder utility via script
    As root call:
    manage find_hostname
    
    (manage is an alias)

---
+++ orthos2/data/management/commands/find_hostname.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+from django.apps import apps
+from django.core.management.base import BaseCommand
+
+from orthos2.utils.hostnamefind import HostnameFinder
+
+class Command(BaseCommand):
+    help = "Find free hostnames\n"
+
+
+    config = apps.get_app_config("data")
+    queries = []
+
+    def __init__(self, *args, **kwargs):
+        super(Command, self).__init__(*args, **kwargs)
+        self.machinesonly = False
+
+    def add_arguments(self, parser):
+        parser.add_argument('--domain', default="arch.suse.de", help="Domainname")
+        parser.add_argument('--arch', default="x86_64", help="architecture")
+        
+    def handle(self, *args, **options):
+        domain = options["domain"]
+        arch = options["arch"]
+        print("Checking for:")
+        print("Domain: {}".format(domain))
+        print("Architecture: {}".format(arch))
+        print()
+        finder = HostnameFinder.by_domain(domain, arch)
+        if not finder:
+            print("HostnameFinder not properly configured")
+            return
+        used, unused = finder.free_hostnames()
+        print("Found {} used machines:\n{}".format(len(used), used))
+        print("Found {} unused machine names:\n{}".format(len(unused), unused))
