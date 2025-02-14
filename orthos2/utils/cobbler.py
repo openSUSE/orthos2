891518177347f74ab381b040e30f96cb8aa5078f
Enno Gotthold <egotthold@suse.de>
2025-02-14 10:26:25 +0100

    Cobbler: Move object ID to method arguments
    This is needed to allow unsaved objects to be edited as get_item_handle doesn't allow unsaved item handles to be returned.

--- orthos2/utils/cobbler.py
+++ orthos2/utils/cobbler.py
@@ -197,13 +197,15 @@ class CobblerServer:
         kernel_options = machine.kernel_options if machine.kernel_options else ""
 
         object_id = self._xmlrpc_server.new_system(self._token)
+        if not isinstance(object_id, str):
+            raise TypeError("Cobbler System ID must be a string!")
         self._xmlrpc_server.modify_system(object_id, "name", machine.fqdn, self._token)
         self._xmlrpc_server.modify_system(
             object_id, "profile", default_profile, self._token
         )
 
         if machine.mac_address:
-            self.add_primary_network_interface(machine)
+            self.add_primary_network_interface(machine, object_id)
         self._xmlrpc_server.modify_system(
             object_id, "filename", get_filename(machine) or "", self._token
         )
@@ -214,11 +216,11 @@ class CobblerServer:
                     object_id, "next_server_v4", ipv4, self._token
                 )
         if machine.has_bmc():
-            self.add_bmc(machine)
+            self.add_bmc(machine, object_id)
         if machine.has_remotepower():
-            self.add_power_options(machine)
+            self.add_power_options(machine, object_id)
         if machine.has_serialconsole():
-            self.add_serial_console(machine)
+            self.add_serial_console(machine, object_id)
         self._xmlrpc_server.modify_system(
             object_id, "kernel_options", kernel_options, self._token
         )
@@ -228,15 +230,18 @@ class CobblerServer:
 
     @login_required
     def add_primary_network_interface(
-        self, machine: "Machine", save: CobblerSaveModes = CobblerSaveModes.SKIP
+        self,
+        machine: "Machine",
+        object_id: str,
+        save: CobblerSaveModes = CobblerSaveModes.SKIP,
     ) -> None:
         """
         Add the primary network interface of the machine to Cobbler.
 
         :param machine: Machine that should be added or updated.
+        :param object_id: ID of object to be added.
         :param save: Whether to save the machine or not.
         """
-        system_handle = self._xmlrpc_server.get_system_handle(machine.fqdn, self._token)
         interface_options = {
             "interfacemaster-default": True,
             "macaddress-default": machine.mac_address,
@@ -247,23 +252,26 @@ class CobblerServer:
             "management-default": True,
         }
         self._xmlrpc_server.modify_system(
-            system_handle, "modify_interface", interface_options, self._token
+            object_id, "modify_interface", interface_options, self._token
         )
         if save != CobblerSaveModes.SKIP:
-            self._xmlrpc_server.save_system(system_handle, self._token, save.value)
+            self._xmlrpc_server.save_system(object_id, self._token, save.value)
 
     @login_required
     def add_bmc(
-        self, machine: "Machine", save: CobblerSaveModes = CobblerSaveModes.SKIP
+        self,
+        machine: "Machine",
+        object_id: str,
+        save: CobblerSaveModes = CobblerSaveModes.SKIP,
     ) -> None:
         """
         Add the BMC of the machine to Cobbler.
 
         :param machine: Machine that should be added or updated.
+        :param object_id: ID of object to be added.
         :param save: Whether to save the machine or not.
         """
         bmc = machine.bmc
-        system_handle = self._xmlrpc_server.get_system_handle(machine.fqdn, self._token)
         interface_options = {
             "interfacetype-bmc": "bmc",
             "macaddress-bmc": bmc.mac,
@@ -272,29 +280,32 @@ class CobblerServer:
             "hostname-bmc": get_hostname(bmc.fqdn),
         }
         self._xmlrpc_server.modify_system(
-            system_handle, "modify_interface", interface_options, self._token
+            object_id, "modify_interface", interface_options, self._token
         )
         if save != CobblerSaveModes.SKIP:
-            self._xmlrpc_server.save_system(system_handle, self._token, save.value)
+            self._xmlrpc_server.save_system(object_id, self._token, save.value)
 
     @login_required
     def add_serial_console(
-        self, machine: "Machine", save: CobblerSaveModes = CobblerSaveModes.SKIP
+        self,
+        machine: "Machine",
+        object_id: str,
+        save: CobblerSaveModes = CobblerSaveModes.SKIP,
     ) -> None:
         """
         Add the Serial Console of the machine to Cobbler.
 
         :param machine: Machine that should be added or updated.
+        :param object_id: ID of object to be added.
         :param save: Whether to save the machine or not.
         """
         console = machine.serialconsole
 
-        system_handle = self._xmlrpc_server.get_system_handle(machine.fqdn, self._token)
         self._xmlrpc_server.modify_system(
-            system_handle, "serial_device", console.kernel_device_num, self._token
+            object_id, "serial_device", console.kernel_device_num, self._token
         )
         self._xmlrpc_server.modify_system(
-            system_handle, "serial_baud_rate", console.baud_rate, self._token
+            object_id, "serial_baud_rate", console.baud_rate, self._token
         )
         if console.kernel_device != "None":
             system_dict = self._xmlrpc_server.get_system(machine.fqdn)
@@ -322,19 +333,23 @@ class CobblerServer:
                 "console"
             ] = f"{console.kernel_device}{console.kernel_device_num},{console.baud_rate}"
             self._xmlrpc_server.modify_system(
-                system_handle, "kernel_options", new_kernel_options, self._token
+                object_id, "kernel_options", new_kernel_options, self._token
             )
         if save != CobblerSaveModes.SKIP:
-            self._xmlrpc_server.save_system(system_handle, self._token, save.value)
+            self._xmlrpc_server.save_system(object_id, self._token, save.value)
 
     @login_required
     def add_power_options(
-        self, machine: "Machine", save: CobblerSaveModes = CobblerSaveModes.SKIP
+        self,
+        machine: "Machine",
+        object_id: str,
+        save: CobblerSaveModes = CobblerSaveModes.SKIP,
     ) -> None:
         """
         Add the out-of-band power management of the machine to Cobbler.
 
         :param machine: Machine that should be added or updated.
+        :param object_id: ID of object to be added.
         :param save: Whether to save the machine or not.
         """
         remotepower = machine.remotepower
@@ -344,57 +359,56 @@ class CobblerServer:
                 "Fence for machine %s couldn't be retrieved via RemotePowerType!"
                 % machine.fqdn
             )
-        system_handle = self._xmlrpc_server.get_system_handle(machine.fqdn, self._token)
 
         self._xmlrpc_server.modify_system(
-            system_handle, "power_type", fence.fence, self._token
+            object_id, "power_type", fence.fence, self._token
         )
 
         if fence.use_identity_file:
             self._xmlrpc_server.modify_system(
-                system_handle, "power_user", fence.username, self._token
+                object_id, "power_user", fence.username, self._token
             )
             self._xmlrpc_server.modify_system(
-                system_handle, "power_identity_file", fence.identity_file, self._token
+                object_id, "power_identity_file", fence.identity_file, self._token
             )
         else:
             username, password = remotepower.get_credentials()
             self._xmlrpc_server.modify_system(
-                system_handle, "power_user", username, self._token
+                object_id, "power_user", username, self._token
             )
             self._xmlrpc_server.modify_system(
-                system_handle, "power_pass", password, self._token
+                object_id, "power_pass", password, self._token
             )
 
         if fence.use_hostname_as_port:
             # The following is ignored since at runtime we will always have a hostname dynamically added.
             self._xmlrpc_server.modify_system(
-                system_handle, "power_id", get_hostname(machine.hostname), self._token  # type: ignore
+                object_id, "power_id", get_hostname(machine.hostname), self._token  # type: ignore
             )
         elif fence.use_port:
             # Temporary workaround until fence raritan accepts port as --plug param
             if fence.fence == "raritan":
                 self._xmlrpc_server.modify_system(
-                    system_handle,
+                    object_id,
                     "power_id",
                     f"system1/outlet{remotepower.port}",
                     self._token,
                 )
             else:
                 self._xmlrpc_server.modify_system(
-                    system_handle, "power_id", remotepower.port, self._token
+                    object_id, "power_id", remotepower.port, self._token
                 )
 
         self._xmlrpc_server.modify_system(
-            system_handle, "power_address", remotepower.get_power_address(), self._token
+            object_id, "power_address", remotepower.get_power_address(), self._token
         )
         if fence.use_options:
             self._xmlrpc_server.modify_system(
-                system_handle, "power_options", remotepower.options, self._token
+                object_id, "power_options", remotepower.options, self._token
             )
 
         if save != CobblerSaveModes.SKIP:
-            self._xmlrpc_server.save_system(system_handle, self._token, save.value)
+            self._xmlrpc_server.save_system(object_id, self._token, save.value)
 
     @login_required
     def _get_cobbler_datastructure(self, machine: "Machine") -> Dict[str, Any]:
