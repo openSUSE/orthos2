ce51a57fba022c47ecc941833c50795c40714acd
Enno Gotthold <egotthold@suse.com>
2025-04-09 09:07:17 +0200

    Remove hardcoded default values

--- orthos2/data/apps.py
+++ orthos2/data/apps.py
@@ -3,8 +3,3 @@ from django.apps import AppConfig
 
 class DataConfig(AppConfig):
     name = "orthos2.data"
-
-    def ready(self) -> None:
-        # prepare types for fast and developer-friendly handling
-        self.get_model("Architecture").Type.prep()  # type: ignore
-        self.get_model("System").Type.prep()  # type: ignore
