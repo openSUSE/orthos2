564f477c6cef459925cafb7bf9ea3232db793d94
Enno Gotthold <enno.gotthold@suse.com>
2025-04-16 09:02:59 +0200

    Settings: Remove ORTHOS_USER check
    This was an artificial limitation that has no technical reason. As long as the user is not changed during the lifetime of the instanc, the application will run just fine.

--- orthos2/taskmanager/management/commands/taskmanager.py
+++ orthos2/taskmanager/management/commands/taskmanager.py
@@ -1,11 +1,7 @@
 import logging
-import os
-import pwd
 import signal
-import sys
 from typing import Any
 
-from django.conf import settings
 from django.core.management.base import BaseCommand, CommandParser
 
 from orthos2.taskmanager.executer import TaskExecuter
@@ -33,16 +29,6 @@ class Command(BaseCommand):
         super(Command, self).__init__(*args, **kwargs)
 
     def handle(self, *args: Any, **options: Any) -> None:
-        run_as_user = pwd.getpwuid(os.getuid())[0]
-        if (not settings.DEBUG) and (run_as_user != settings.RUN_AS_USER):
-            logger.error(
-                "TaskManager needs to be run as user '%s', not '%s'! Exit.",
-                settings.RUN_AS_USER,
-                run_as_user,
-            )
-            sys.exit(1)
-        logger.info("TaskManager runs as '%s'...", run_as_user)
-
         if options["start"]:
             signal.signal(signal.SIGTERM, handler)
             signal.signal(signal.SIGUSR1, handler)
