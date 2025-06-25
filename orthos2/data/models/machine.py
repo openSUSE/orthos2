a9c458e3d0cd68d74c3a280c2f6acdf2178ebe5d
Enno Gotthold <enno.gotthold@suse.com>
2025-06-25 16:27:25 +0200

    Machine: Don't attempt to create Domain
    This isn't possible due to missing required information.

--- orthos2/data/models/machine.py
+++ orthos2/data/models/machine.py
@@ -784,10 +784,7 @@ class Machine(models.Model):
             raise ValidationError(
                 "{} systems cannot use a BMC".format(self.system.name)
             )
-        # create & assign network domain and ensure that the FQDN always matches the fqdn_domain
-        domain, created = Domain.objects.get_or_create(name=get_domain(self.fqdn))
-        if created:
-            domain.save()
+        domain = Domain.objects.get(name=get_domain(self.fqdn))
         self.fqdn_domain = domain
 
         # create & assign enclosure according to naming convention if no enclosure given
