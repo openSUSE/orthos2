520926c35d5d7b428677f80e677028a4416551cf
Olaf Hering <olaf@aepfle.de>
2025-11-26 18:36:03 +0100

    Meta: remove stale get_installation
    This function was not called because /etc/SuSE-release was
    removed in SLE15. The expected partition layout is not used
    on all machines. The partitions were mounted read-write for
    no obvious reasons. Remove also the stale function chroot_kernel.
    
    Signed-off-by: Olaf Hering <olaf@aepfle.de>

--- orthos2/meta/data/scripts/machine_get_installations.sh
+++ orthos2/meta/data/scripts/machine_get_installations.sh
@@ -72,51 +72,7 @@ function pretty_os()                                                       # {{{
     echo "$dist"
 }                                                                          # }}}
 
-function chroot_kernel()                                                   # {{{
-{
-    local chroot=$1
-
-    rpm -r ${chroot} -qa kernel-* \
-        | grep -v -- -source- \
-        | grep -v -- -update-tool \
-        | grep -v -- -sym- \
-        | grep -v -- -kdump- \
-        | grep -v -- -debuginfo- \
-        | grep -v -- -debugsource- \
-        | head -1 \
-        | sed -e 's/kernel-//g' \
-        | sed -e 's/\([a-z]\+\)-\(.*\)/\2-\1/'
-}                                                                          # }}}
-
-function get_installation()                                                # {{{
-{
-    local part=$1
-    local mp=$2
-
-    if [ -z "$mp" ] ; then
-        echo >&2 "mounting $part failed, and its currently not mounted"
-        return
-    fi
-
-    if [ -f $mp/etc/SuSE-release ] ; then
-        echo ----
-        if grep i[365]86 $mp/etc/SuSE-release ; then
-            echo ARCH=i386
-        else
-            echo ARCH=$GLOBAL_ARCH
-        fi
-        echo KERNEL=$(chroot_kernel $mp)
-        DIST=$(pretty_os $mp)
-        MILESTONE=$(get_milestone $mp/etc/issue)
-        echo DIST=${DIST} ${MILESTONE}
-        echo PART=${part}
-        echo RUNNING=0
-    fi
-}                                                                          # }}}
 GLOBAL_ARCH=$(uname -i)
-
-DISKS=$(cat /proc/partitions |grep -v loop | awk '{print $4}' | \
-    grep -v name | grep -v '^$' | grep '[0-9]')
 ROOT=$(mount | grep ' / ' | cut -d ' ' -f 1)
 
 #
@@ -129,42 +85,5 @@ MILESTONE=$(get_milestone /etc/issue)
 echo DIST=${DIST} ${MILESTONE}
 echo RUNNING=1
 echo PART=${ROOT}
-
-for partition in ${DISKS} ; do
-    partition=/dev/${partition}
-    # Only try filesystems which had been default for the
-    # corresponding partitions
-    if [ ! -b ${partition} ] || [ ${partition} = ${ROOT} ] ; then
-	continue;
-    fi
-    FS=""
-    case "${partition}" in
-	*5)
-	    FS="ext3"
-	    ;;
-	*6)
-	    FS="ext3"
-	    ;;
-	*7)
-	    FS="ext4 ext3 btrfs"
-	    ;;
-	*8)
-	    FS="ext3"
-	    ;;
-	*)
-	    continue
-	    ;;
-    esac
-    for fs in $FS;do
-	mount -t "$fs" ${partition} /mnt &> /dev/null
-	if [ $? -ne 0 ];then
-	    continue;
-	fi
-	if [ -f /etc/SuSE-release ] && [ -f /mnt/etc/motd ] ; then
-            get_installation $partition /mnt
-	fi
-	umount /mnt &> /dev/null
-    done
-done
 
 # vim: set sw=4 ts=4 et: :collapseFolds=1:
